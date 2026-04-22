844a3e3ee857f0d07a7ae0a262ed748ab897d6b1
Andresmejia62 <amejia@uamv.edu.ni>
2026-04-22 15:48:10 -0600

    Correciones

--- src/main/java/uam/Main.java
+++ src/main/java/uam/Main.java
@@ -4,13 +4,15 @@ import uam.servicios.ProductoServicio;
 
 
 public class Main {
-    static void main() {
-        ProductoServicio productos = new ProductoServicio();
-        productos.agregarProducto("Cafe", 40, 3);
-        productos. agregarProducto("Churro", 45, 2);
+    public static void main(String[] args) {
 
-        System.out.println("Factura");
-        System.out.println(productos.getProductos());
-        System.out.println("Total a pagar " + productos.getMonto());
+            ProductoServicio productos = new ProductoServicio();
+            productos.agregarProducto("Cafe", 40, 3);
+            productos. agregarProducto("Churro", 45, 2);
+
+            System.out.println("Factura");
+            System.out.println(productos.getProductos());
+            System.out.println("Total a pagar " + productos.getMonto());
+        }
     }
-}
+
