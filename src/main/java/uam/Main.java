1bca461117a3c893ba31189db128058891ac9e90
Andresmejia62 <amejia@uamv.edu.ni>
2026-04-22 15:33:22 -0600

    Modificaciones

--- src/main/java/uam/Main.java
+++ src/main/java/uam/Main.java
@@ -7,10 +7,10 @@ public class Main {
     static void main() {
         ProductoServicio productos = new ProductoServicio();
         productos.agregarProducto("Cafe", 40, 3);
-        productos. agregarProducto("Tajadas con queso", 60, 3);
+        productos. agregarProducto("Churro", 45, 2);
 
         System.out.println("Factura");
         System.out.println(productos.getProductos());
-        System.out.println("Total pagar " + productos.getMonto());
+        System.out.println("Total a pagar " + productos.getMonto());
     }
 }
